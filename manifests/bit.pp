e2e9240d50c6aebc8bbeda7faf75e7b53ed0e612
Matt Palmer <mpalmer@hezmatt.org>
2014-08-15 09:24:28 +1000

    Dear Puppet docs, you're a fucking liar
    https://docs.puppetlabs.com/puppet/latest/reference/lang_defined_types.html#metaparameters sez:
    
    Every resource contained in the instance will also have that metaparameter.
    
    Except... it doesn't.  So we need to pass it manually.  Good job, Puppet.

--- manifests/bit.pp
+++ manifests/bit.pp
@@ -4,8 +4,12 @@ define bitfile::bit(
 		$ordinal = 50
 ) {
 	underscore_bit_file_bit { $name:
-		path    => $path,
-		ordinal => $ordinal,
-		content => $content,
+		path      => $path,
+		ordinal   => $ordinal,
+		content   => $content,
+		before    => $before,
+		require   => $require,
+		notify    => $notify,
+		subscribe => $subscribe,
 	}
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,12 @@ define bitfile(
 		$group = undef
 ) {
 	underscore_bit_file { $name:
-		mode => $mode,
-		owner => $owner,
-		group => $group,
+		mode      => $mode,
+		owner     => $owner,
+		group     => $group,
+		before    => $before,
+		require   => $require,
+		notify    => $notify,
+		subscribe => $subscribe,
 	}
 }
